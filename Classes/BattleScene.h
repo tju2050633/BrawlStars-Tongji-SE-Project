907df1d0b0fc3ed512924a1d53487235be4cf34f
li-peiyun <1980234017@qq.com>
2022-05-27 21:36:11 +0800

    战斗地图场景
    添加：战斗地图绘制与使用
    功能：物理碰撞、草丛显示等
    说明：文件夹TileGameResources需放入Resources文件夹中
    补充：/**********/标识的部分需调用其他类中的函数

---
+++ Classes/BattleScene.h
@@ -0,0 +1,36 @@
+#ifndef __BATTLESCENE_H
+#define __BATTLESCENE_H
+
+#include "cocos2d.h"
+USING_NS_CC; //编译器使用cocos2d命名空间，省略cocos2d::
+
+/* 战斗场景类 */
+class BattleScene : public Layer
+{
+public:
+	static Scene* createScene();
+	CREATE_FUNC(BattleScene);
+	virtual bool init();
+	/****************************
+		 玩家移动：完成后需替换
+	*******************************/
+	void playerMove(); //玩家移动函数
+	virtual void setViewPointCenter(Point position); //设置窗口镜头位置
+	Point tileCoordForPosition(Point position); //转化为tile坐标
+	void setPlayerPosition(Point position); //设置玩家位置（添加物理碰撞）
+	void setGrassOpacity(Point position); //设置草丛透明度
+	void breakWall(Point position); //墙壁被大招摧毁
+
+private:
+	TMXTiledMap* _tileMap; //战斗地图.tmx格式
+	TMXLayer* _background; //背景层
+	TMXLayer* _wall; //墙壁图层
+	Sprite* _wallCell; //墙壁单元格
+	Sprite* _player; //玩家
+	TMXLayer* _meta; //属性图层
+	TMXLayer* _grass; //草丛图层
+	Sprite* _grassCell; //草丛单元格
+};
+
+
+#endif __BATTLESCENE_H
